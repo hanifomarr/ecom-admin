ef488cf209fdd6c9d4cd6b1a55a6a8a2d7c97583
Hanif Omar <hanif.omarr@gmail.com>
2024-09-28 12:25:00 +0800

    feat(router): add react-router-dom for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,37 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Dashboard } from "./routes";
+import { Root } from "./routes/root";
+import { Products } from "./routes/product";
+import { Customers } from "./routes/customers";
+import { Orders } from "./routes/orders";
+
 function App() {
-  return <div>Homepage</div>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+      children: [
+        {
+          index: true,
+          element: <Dashboard />,
+        },
+        {
+          path: "products",
+          children: [{ index: true, element: <Products /> }],
+        },
+        {
+          path: "customers",
+          element: <Customers />,
+        },
+        {
+          path: "orders",
+          element: <Orders />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
